b5d934b8142d651c5de04902e599b2cf8a806177
AnotherOne661 <deathcannon661@gmail.com>
2024-11-06 18:33:11 +0100

    Changed variables

--- public/index.php
+++ public/index.php
@@ -1,13 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Arcadia - Tu tienda TCG</title>
-</head>
 
-<body>
   <?php
 
   require_once "helper.php";
@@ -43,6 +37,5 @@
   }
   
   ?>
-</body>
 
 </html>
