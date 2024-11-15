f145a4cd22e2f54a850c598395b0cb635561157f
Another One <159118983+AnotherOne661@users.noreply.github.com>
2024-11-16 00:08:01 +0100

    Delete Controllers directory

--- Controllers/collectionRouter.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../MODELS/connection.php';
-require_once __DIR__ . '/../MODELS/Model.php';
-
-$model = new Model();
-$fetch = $model->getAllResultsCollections();
-
-header('Content-Type: application/json');
-echo json_encode($fetch);
