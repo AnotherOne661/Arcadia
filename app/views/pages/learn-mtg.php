d9f71b99223c87d5b06d8da70a349fc51bb80a47
AnotherOne661 <deathcannon661@gmail.com>
2025-01-22 13:10:34 +0100

    UPDATED DB

--- app/views/pages/learn-mtg.php
+++ app/views/pages/learn-mtg.php
@@ -2,19 +2,21 @@
   <section class="learn-mtg">
     <h1>Bienvenido a la Guía de Magic: The Gathering</h1>
     <div class="learn-content">
-      <p>Magic: The Gathering (MTG) es un juego de cartas coleccionables en el que los jugadores asumen el papel de
+      <p class="learn-p">Magic: The Gathering (MTG) es un juego de cartas coleccionables en el que los jugadores asumen
+        el papel de
         poderosos magos, conocidos como Planeswalkers, que luchan por el control de un multiverso. En esta guía te
         explicamos las reglas básicas y los formatos más populares de este juego.</p>
       <div class="card-example">
         <img src="assets/images/struc/land_footer/black-lotus.png" alt="Carta Black Lotus">
-        <p>Una de las cartas más famosas de Magic: The Gathering: Black Lotus.</p>
+        <p class="learn-p">Una de las cartas más famosas de Magic: The Gathering: Black Lotus.</p>
       </div>
     </div>
 
     <!-- Normas Básicas -->
     <section class="basic-rules">
       <h2>Normas Básicas</h2>
-      <p>Magic: The Gathering es un juego de dos o más jugadores que luchan utilizando mazos de cartas. Cada jugador
+      <p class="learn-p">Magic: The Gathering es un juego de dos o más jugadores que luchan utilizando mazos de cartas.
+        Cada jugador
         tiene un mazo que puede contener criaturas, hechizos, tierras y artefactos, y el objetivo es reducir los puntos
         de vida del oponente a cero.</p>
       <ul>
@@ -30,7 +32,8 @@
     <!-- Jugadores -->
     <section class="players">
       <h2>Jugadores</h2>
-      <p>El juego se juega entre dos o más jugadores. Cada jugador construye su mazo con al menos 60 cartas. Las cartas
+      <p class="learn-p">El juego se juega entre dos o más jugadores. Cada jugador construye su mazo con al menos 60
+        cartas. Las cartas
         pueden incluir criaturas, hechizos, tierras, artefactos y encantamientos.</p>
     </section>
 
@@ -49,11 +52,13 @@
     <!-- Formatos de Juego -->
     <section class="formats">
       <h2>Formatos de Juego</h2>
-      <p>Magic: The Gathering ofrece varios formatos, cada uno con reglas y restricciones propias. A continuación, te
+      <p class="learn-p">Magic: The Gathering ofrece varios formatos, cada uno con reglas y restricciones propias. A
+        continuación, te
         explicamos los formatos más populares:</p>
 
       <h3>Standard</h3>
-      <p>En el formato Standard, los jugadores deben construir su mazo usando cartas de las colecciones más recientes.
+      <p class="learn-p">En el formato Standard, los jugadores deben construir su mazo usando cartas de las colecciones
+        más recientes.
         Este formato se juega en torneos oficiales y es el más común para los jugadores que participan en eventos
         competitivos.</p>
       <ul>
@@ -64,7 +69,8 @@
       </ul>
 
       <h3>Commander</h3>
-      <p>En el formato Commander, los jugadores construyen mazos de 100 cartas, con un único comandante, que es una
+      <p class="learn-p">En el formato Commander, los jugadores construyen mazos de 100 cartas, con un único comandante,
+        que es una
         carta legendaria que define la estrategia del mazo. Este formato se juega principalmente de forma casual, pero
         también tiene una gran comunidad competitiva.</p>
       <ul>
@@ -75,7 +81,8 @@
       </ul>
 
       <h3>Pioneer</h3>
-      <p>El formato Pioneer permite a los jugadores usar cartas de todas las colecciones desde la expansión de Return to
+      <p class="learn-p">El formato Pioneer permite a los jugadores usar cartas de todas las colecciones desde la
+        expansión de Return to
         Ravnica en adelante. Este formato es relativamente nuevo, pero ha ganado mucha popularidad debido a su
         accesibilidad.</p>
       <ul>
@@ -86,7 +93,8 @@
       </ul>
 
       <h3>Draft</h3>
-      <p>El formato Draft es un estilo de juego en el que los jugadores abren paquetes de cartas y seleccionan una carta
+      <p class="learn-p">El formato Draft es un estilo de juego en el que los jugadores abren paquetes de cartas y
+        seleccionan una carta
         por turno para agregarla a su mazo. Este formato se juega comúnmente en eventos de lanzamiento de nuevas
         colecciones.</p>
       <ul>
@@ -100,7 +108,8 @@
     <!-- Video Tutorial -->
     <section class="video-tutorial">
       <h2>Video Tutorial</h2>
-      <p>Para aprender a jugar Magic: The Gathering de manera más detallada, te dejamos este video tutorial:</p>
+      <p class="learn-p">Para aprender a jugar Magic: The Gathering de manera más detallada, te dejamos este video
+        tutorial:</p>
       <div class="video-container">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/68XopFdbxx0" frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
--- app/views/pages/learn-ygo.php
+++ app/views/pages/learn-ygo.php
@@ -2,46 +2,56 @@
   <section class="learn-ygo">
     <h1>Bienvenido a la Guía de Yu-Gi-Oh!</h1>
     <div class="learn-content">
-      <p>En esta página encontrarás recursos, tutoriales y guías para aprender a jugar Yu-Gi-Oh! Colecciona cartas,
+      <p class="learn-p">En esta página encontrarás recursos, tutoriales y guías para aprender a jugar Yu-Gi-Oh!
+        Colecciona cartas,
         construye mazos y compite en emocionantes duelos.</p>
       <div class="card-example">
         <img src="assets/images/struc/land_footer/dark-magician.png" alt="Dark Magician">
-        <p>Una de las cartas más icónicas de Yu-Gi-Oh! - El Mago Oscuro.</p>
+        <p class="learn-p">Una de las cartas más icónicas de Yu-Gi-Oh! - El Mago Oscuro.</p>
       </div>
     </div>
 
     <!-- Normas Básicas -->
     <section class="basic-rules">
       <h2>Normas Básicas</h2>
-      <p>Yu-Gi-Oh! es un juego de cartas donde dos jugadores luchan con mazos de cartas que representan monstruos,
+      <p class="learn-p">Yu-Gi-Oh! es un juego de cartas donde dos jugadores luchan con mazos de cartas que representan
+        monstruos,
         hechizos y trampas. El objetivo es reducir los puntos de vida del oponente a cero.</p>
-      <ul>
-        <li><strong>Fase de Inicio:</strong> Los jugadores colocan sus cartas en el campo y se preparan para el duelo.
+      <ul class=".learn-ul">
+        <li class="learn-li"><strong>Fase de Inicio:</strong> Los jugadores colocan sus cartas en el campo y se preparan
+          para el duelo.
         </li>
-        <li><strong>Fase de Juego:</strong> Los jugadores alternan turnos, invocando monstruos y activando cartas.
+        <li class="learn-li"><strong>Fase de Juego:</strong> Los jugadores alternan turnos, invocando monstruos y
+          activando cartas.
         </li>
-        <li><strong>Fase de Batalla:</strong> Durante esta fase, los monstruos pueden atacar al oponente.</li>
-        <li><strong>Fase Final:</strong> El jugador finaliza su turno y el oponente comienza el siguiente.</li>
+        <li class="learn-li"><strong>Fase de Batalla:</strong> Durante esta fase, los monstruos pueden atacar al
+          oponente.</li>
+        <li class="learn-li"><strong>Fase Final:</strong> El jugador finaliza su turno y el oponente comienza el
+          siguiente.</li>
       </ul>
     </section>
 
     <!-- Jugadores -->
     <section class="players">
       <h2>Jugadores</h2>
-      <p>El juego se juega entre dos jugadores, cada uno con un mazo de al menos 40 cartas. Cada jugador comienza con
+      <p class="learn-p">El juego se juega entre dos jugadores, cada uno con un mazo de al menos 40 cartas. Cada jugador
+        comienza con
         8000 puntos de vida.</p>
-      <p>El objetivo es reducir los puntos de vida del oponente a cero, utilizando monstruos, hechizos y trampas para
+      <p class="learn-p">El objetivo es reducir los puntos de vida del oponente a cero, utilizando monstruos, hechizos y
+        trampas para
         vencerlo.</p>
     </section>
 
     <!-- Requisitos -->
     <section class="requirements">
       <h2>Requisitos para Jugar</h2>
-      <ul>
-        <li><strong>Cartas:</strong> Cada jugador necesita un mazo de cartas de Yu-Gi-Oh!, con un mínimo de 40 cartas.
+      <ul class=".learn-ul">
+        <li class="learn-li"><strong>Cartas:</strong> Cada jugador necesita un mazo de cartas de Yu-Gi-Oh!, con un
+          mínimo de 40 cartas.
         </li>
-        <li><strong>Puntos de vida:</strong> Ambos jugadores comienzan con 8000 puntos de vida.</li>
-        <li><strong>Un campo de juego:</strong> Un espacio donde colocar las cartas, generalmente con una zona para
+        <li class="learn-li"><strong>Puntos de vida:</strong> Ambos jugadores comienzan con 8000 puntos de vida.</li>
+        <li class="learn-li"><strong>Un campo de juego:</strong> Un espacio donde colocar las cartas, generalmente con
+          una zona para
           monstruos, hechizos y trampas.</li>
       </ul>
     </section>
@@ -49,7 +59,8 @@
     <!-- Video Tutorial -->
     <section class="video-tutorial">
       <h2>Video Tutorial</h2>
-      <p>Para aprender las reglas de Yu-Gi-Oh! y cómo jugar de manera más detallada, te dejamos este video tutorial:
+      <p class="learn-p">Para aprender las reglas de Yu-Gi-Oh! y cómo jugar de manera más detallada, te dejamos este
+        video tutorial:
       </p>
       <div class="video-container">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/6gBhDjkTLm8" frameborder="0"
