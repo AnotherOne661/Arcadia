c548b3a34219bb0691dc0f5f4a3771871e37ee52
AnotherOne661 <deathcannon661@gmail.com>
2025-01-22 09:24:34 +0100

    Half calendar

---
+++ app/repositories/CalendarRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+// Requerimos el repositorio principal
+require_once __DIR__ . '/Repository.php';
+
+// Requerimos los modelos necesarios
+require_once __DIR__ . '/../models/Collection.php';
+
+// Extendemos HomeRepository de Repository
+class CalendarRepository extends Repository
+{
+  public function __construct()
+  {
+    // Llamamos al constructor padre y, al atributo del padre
+    // le damos el atributo que es el nombre de la tabla sobre la que trabajaremos
+    parent::__construct();
+
+    $this->tableName = 'eventos';
+  }
+
+  // Método para buscar un test por su ID y devolverlo como un objeto Test
+
+}
