adfd08bf5fc8326f0f50bcdd7b79ef7409b17d07
AnotherOne661 <deathcannon661@gmail.com>
2025-02-06 17:15:26 +0100

    robots and htaccess done

--- app/views/pages/error.php
+++ app/views/pages/error.php
@@ -1,8 +1,10 @@
 <div class="error-container">
   <div class="error-message">
     <h1 class="error-code">Oops!</h1>
-    <p class="error-text">Something went wrong. We're sorry, but the page you're looking for doesn't exist.</p>
-    <a href="/" class="btn-go-home">Go Back to Home</a>
+    <h2 class="error-code">Error 404</h2>
+    <p class="error-text">¡El Reino De Las Sombras Se Ha Apoderado de la Página Que Buscas!
+    </p>
+    <a href="/" class="btn-go-home">¡Corre, insensato!</a>
   </div>
   <div class="error-image">
     <img src="/assets/images/struc/error/dark-magician.png" alt="Error Image">
