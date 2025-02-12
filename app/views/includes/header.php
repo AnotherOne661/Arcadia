fe43aaeb8400bda39eec68d8e5977cee6ff0641c
AnotherOne661 <deathcannon661@gmail.com>
2025-02-12 10:18:59 +0100

    tryout

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -35,8 +35,7 @@
                 <div class="offcanvas-header">
                     <h5 id="offcanvasRightLabel" class="menuTitle"></h5>
                     <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
-                        aria-label="Close"><img src="<?php echo
-                            $ABS_URL; ?>image/icons/close.jpg" alt="Cerrar"></button>
+                        aria-label="Close"></button>
 
 
                 </div>
